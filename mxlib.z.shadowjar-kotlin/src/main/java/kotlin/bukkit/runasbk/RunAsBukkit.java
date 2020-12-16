3f97dd4a394d085b9edee68d6cce6b166d3df6ed
Karlatemp <karlatemp@vip.qq.com>
2020-12-16 11:26:23 +0800

    KotlinRuntime shadow

---
+++ mxlib.z.shadowjar-kotlin/src/main/java/kotlin/bukkit/runasbk/RunAsBukkit.java
@@ -0,0 +1,10 @@
+package kotlin.bukkit.runasbk;
+
+import kotlin.DeprecationLevel;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@java.lang.Deprecated
+@kotlin.Deprecated(level = DeprecationLevel.ERROR, message = "")
+public class RunAsBukkit extends JavaPlugin {
+    // Do Nothing
+}
